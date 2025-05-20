cbf3d1c9a21e599847a50420ea3a3ce8fdb232da
juarex9 <agustinjuarez375@gmail.com>
2025-05-19 21:40:13 -0300

    preentrega-2

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,8 +1,29 @@
 import { Flex } from "@chakra-ui/react";
+import { getAllProducts } from "../services/products.services";
+import { useEffect, useState } from "react";
 
-const ItemListContainer = ({greeting}) => {
-    return <Flex height="93%" fontSize="2rem" alignItems="center" justifyContent="center">{greeting}</Flex>;
+
+const ItemListContainer = ({ greeting }) => {
+
+    const [products, setProducts] = useState();
+
+    useEffect(() => {
+        getAllProducts().then((res) => {
+            setProducts(res.data.products);
+        });
+
+    }, []);
+
+    return (
+        <Flex>
+            {products.map((product) => {
+                return <div>{product.title}</div>;
+            })}
+        </Flex>
+    );
 };
+
+
 
 
 export default ItemListContainer;
